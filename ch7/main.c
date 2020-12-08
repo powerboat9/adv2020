09469c489af6c26c4b72127d6af3f3c149a63125
powerboat9 <powerboat9.gamer@gmail.com>
2020-12-07 19:31:07 -0500

    Improved code

--- ch7/main.c
+++ ch7/main.c
@@ -31,14 +31,16 @@ FILE *map_file(const char *filename) {
 
 #define MAX_DATA 4096
 
+struct item_entry {
+    uint32_t bag_id;
+    uint32_t bag_cnt;
+};
+
 struct bag {
     uint32_t bag_id;
     int32_t p1_state;
     uint32_t contain_cnt;
-    struct {
-        uint32_t bag_id;
-        uint32_t bag_cnt;
-    } contain[16];
+    struct item_entry contain[16];
 };
 
 struct bag data[MAX_DATA];
@@ -58,7 +60,6 @@ int compare_bag(const void *a, const void *b) {
 }
 
 uint32_t scan_color(char **s) {
-    char *orig = *s;
     uint32_t acc = 0;
     while (**s != ' ') {
         acc = ((acc << 5) - acc) ^ *((*s)++);
@@ -72,21 +73,15 @@ uint32_t scan_color(char **s) {
 }
 
 int find_bag_idx(uint32_t bag_id) {
-/*
     int min = 0;
     int max = data_len - 1;
-    printf("finding %u\n", bag_id);
     while (1) {
-        printf("picking...\n");
         int pick = (min + max) / 2;
         if (data[pick].bag_id == bag_id) return pick;
         else if (min == max) __builtin_trap();
         else if (data[pick].bag_id < bag_id) min = pick + 1;
         else if (data[pick].bag_id > bag_id) max = pick - 1;
     }
-*/
-    for (int i = 0; i < data_len; i++) if (data[i].bag_id == bag_id) return i;
-    __builtin_trap();
 }
 
 static uint32_t shiny_gold_hash;
@@ -100,7 +95,6 @@ int is_p1_mark(int id) {
 
 int is_p1_mark_idx(int idx) {
     if (data[idx].p1_state == -1) {
-        int acc = 0;
         for (int i = 0; i < data[idx].contain_cnt; i++) {
             if (is_p1_mark(data[idx].contain[i].bag_id)) {
                 return data[idx].p1_state = 1;
@@ -137,17 +131,19 @@ int main(int argc, char **argv) {
         if (!strcmp(ptr, "no other bags.\n")) {
             data[data_len++] = (struct bag) {.bag_id = head_id, .p1_state = 0, .contain_cnt = 0};
         } else {
-            data[data_len] = (struct bag) {.bag_id = head_id, .p1_state = -1, .contain_cnt = 0};
+            struct bag *cur_bag = data + data_len;
+            *cur_bag = (struct bag) {.bag_id = head_id, .p1_state = -1, .contain_cnt = 0};
             do {
                 while (*ptr < '0') ptr++;
-                data[data_len].contain[data[data_len].contain_cnt].bag_cnt = 0;
+                struct item_entry *cur_ent = cur_bag->contain + (cur_bag->contain_cnt++);
+                cur_ent->bag_cnt = 0;
                 while (*ptr != ' ') {
-                    data[data_len].contain[data[data_len].contain_cnt].bag_cnt *= 10;
-                    data[data_len].contain[data[data_len].contain_cnt].bag_cnt += *ptr - '0';
+                    cur_ent->bag_cnt *= 10;
+                    cur_ent->bag_cnt += *ptr - '0';
                     ptr++;
                 }
                 ptr++;
-                data[data_len].contain[data[data_len].contain_cnt++].bag_id = scan_color(&ptr);
+                cur_ent->bag_id = scan_color(&ptr);
                 ptr += 4;
                 if (*ptr == 's') ptr++;
             } while (*ptr != '.');
