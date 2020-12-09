83cdfb5262f6b7825e6b63c8e6f9975945ba9b76
powerboat9 <powerboat9.gamer@gmail.com>
2020-12-08 20:08:46 -0500

    Started trying to get it to work with a bigger dataset

--- ch8/main.c
+++ ch8/main.c
@@ -7,6 +7,7 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <setjmp.h>
 
 void die(char *str) {
     fprintf(stderr, "[ERROR] %s\n", str);
@@ -29,7 +30,7 @@ FILE *map_file(const char *filename) {
     return fmemopen(data, fd_stat.st_size, "r");
 }
 
-#define MAX_DATA 4096
+#define MAX_DATA (1024L * 1024L * 32L)
 
 #define INS_ACC 0
 #define INS_JMP 1
@@ -40,35 +41,80 @@ struct ins {
     uint32_t val;
 };
 
+struct pause_data {
+    uint32_t mark;
+    uint32_t has_flipped;
+    uint32_t source;
+    uint32_t insta_block;
+};
+
 struct ins asm_data[MAX_DATA];
-int asm_mark[MAX_DATA];
-int asm_len = 0;
+struct pause_data asm_pause[MAX_DATA];
+long asm_len = 0;
+long asm_idx = 0;
 
 #define MAX_LINE 256
 
-int exec_asm() {
-    int pos = 0, acc = 0;
-    while (1) {
-        if ((pos > asm_len) || (pos < 0)) return 0;
-        if (pos == asm_len) {
-            printf("P2: %d\n", acc);
-            exit(0);
-        } else if (asm_mark[pos]) {
-            memset(asm_mark, 0, sizeof(asm_mark));
-            return acc;
-        }
-        asm_mark[pos] = 1;
-        switch (asm_data[pos].op) {
-            case INS_ACC: acc += asm_data[pos].val; pos++; break;
-            case INS_JMP: pos += asm_data[pos].val; break;
-            case INS_NOP: pos++; break;
-        }
+int exec_p1(int pos, int acc) {
+    if ((pos > asm_len) || (pos < 0)) die("ip overflow");
+    if (pos == asm_len) die("unexpected success");
+    if (asm_mark[pos]) return acc;
+    asm_mark[pos] = 1;
+    int pos_n, acc_n;
+    switch (asm_data[pos].op) {
+        case INS_ACC: acc_n = acc + asm_data[pos].val; pos_n = pos + 1; break;
+        case INS_JMP: pos_n = pos + asm_data[pos].val; break;
+        case INS_NOP: pos_n = pos + 1; break;
+    }
+    printf("NEXT\n");
+    int r = exec_p1(pos_n, acc_n);
+    asm_mark[pos] = 0;
+    return r;
+}
+
+int exec_p2(int pos, int acc, jmp_buf fail, int may_flip) {
+    if ((pos > asm_len) || (pos < 0)) longjmp(fail, 1);
+    else if (pos == asm_len) return acc;
+    else if (asm_mark[pos]) longjmp(fail, 1);
+    asm_mark[pos] = 1;
+    jmp_buf my_fail;
+    int r;
+    switch (asm_data[pos].op) {
+        case INS_ACC:
+            if (setjmp(my_fail)) {
+                asm_mark[pos] = 0;
+                longjmp(fail, 1);
+            } else {
+                return exec_p2(pos + 1, acc + asm_data[pos].val, my_fail, may_flip);
+            }
+        case INS_JMP:
+            if (setjmp(my_fail)) {
+                if (!may_flip || setjmp(my_fail)) {
+                    asm_mark[pos] = 0;
+                    longjmp(fail, 1);
+                } else {
+                    return exec_p2(pos + 1, acc, my_fail, 0);
+                }
+            } else {
+                return exec_p2(pos + asm_data[pos].val, acc, my_fail, may_flip);
+            }
+        case INS_NOP:
+            if (setjmp(my_fail)) {
+                if (!may_flip || setjmp(my_fail)) {
+                    asm_mark[pos] = 0;
+                    longjmp(fail, 1);
+                } else {
+                    return exec_p2(pos + asm_data[pos].val, acc, my_fail, 0);
+                }
+            } else {
+                return exec_p2(pos + 1, acc, my_fail, may_flip);
+            }
     }
 }
 
 int main(int argc, char **argv) {
     // read input
-    FILE *fd = map_file("test.txt");
+    FILE *fd = map_file("bb.txt");
     char line[MAX_LINE];
     while (fgets(line, MAX_LINE, fd) != NULL) {
         char optxt[8];
@@ -83,12 +129,7 @@ int main(int argc, char **argv) {
             case 'n': asm_data[asm_len++].op = INS_NOP; break;
         }
     }
-    printf("P1: %d\n", exec_asm());
-    for (int i = 0; i < asm_len; i++) {
-        if (asm_data[i].op == INS_ACC) continue;
-        asm_data[i].op ^= 3;
-        exec_asm();
-        asm_data[i].op ^= 3;
-    }
+    printf("P1: %d\n", exec_p1(0, 0));
+    printf("P2: %d\n", exec_p2(0, 0, NULL, 1));
     return 0;
 }
